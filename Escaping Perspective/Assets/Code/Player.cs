0eba3d830730231d743cbad3f3e344a38b5b15c7
stephanrb3 <stephanrb3@gmail.com>
2018-04-07 16:26:10 -0400

    mood: make game

---
+++ Escaping Perspective/Assets/Code/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject human;
+    public GameObject cow;
+    public GameObject frog;
+    public GameObject mouse;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 0.5F;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
